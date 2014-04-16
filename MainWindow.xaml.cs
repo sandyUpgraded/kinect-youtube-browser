3627cc1d4164b96b269b25120c530d6cce13c493
Sandy K <psandyk@gmail.com>
2014-04-15 22:48:51 -0700

    original prototype

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,429 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.Slideshow
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+    using System.Windows.Controls;
+    using Microsoft.Kinect;
+    using Microsoft.Samples.Kinect.SwipeGestureRecognizer;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The recognizer being used.
+        /// </summary>
+        private readonly Recognizer activeRecognizer;
+
+
+        /// <summary>
+        /// The sensor we're currently tracking.
+        /// </summary>
+        private KinectSensor nui;
+
+        /// <summary>
+        /// There is currently no connected sensor.
+        /// </summary>
+        private bool isDisconnectedField = true;
+
+        /// <summary>
+        /// Any message associated with a failure to connect.
+        /// </summary>
+        private string disconnectedReasonField;
+
+        /// <summary>
+        /// Array to receive skeletons from sensor, resize when needed.
+        /// </summary>
+        private Skeleton[] skeletons = new Skeleton[0];
+
+        /// <summary>
+        /// Time until skeleton ceases to be highlighted.
+        /// </summary>
+        private DateTime highlightTime = DateTime.MinValue;
+
+        /// <summary>
+        /// The ID if the skeleton to be tracked.
+        /// </summary>
+        private int nearestId = -1;
+
+        /// <summary>
+        /// The index of the current image.
+        /// </summary>
+        private int indexField = 1;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow" /> class.
+        /// </summary>
+        public MainWindow()
+        {
+            /***************   NEED TO CHANGE PICTURE HERE  &*********/
+
+            InitializeComponent();
+
+            // Create the gesture recognizer.
+            this.activeRecognizer = this.CreateRecognizer();
+
+            this.next.Source = new Uri("http://www.cnn.com/video/#/video/bestoftv/2012/09/23/nr-green-day-singer-meltdown-sot.cnn");
+            this.previous.Source = new Uri("http://www.cnn.com/video/#/video/bestoftv/2012/09/23/nr-green-day-singer-meltdown-sot.cnn");
+            this.current.Source = new Uri("http://www.cnn.com/video/#/video/us/2012/09/21/pkg-hi-tsunami-floating-dock.kgmb-khnl");
+
+            //FOR TEST PURPOSES
+            this.rotatorBtn.Click += rotatorBtn_Click;
+
+            // Wire-up window loaded event.
+            Loaded += this.OnMainWindowLoaded;
+        }
+
+        void rotatorBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs("PreviousPicture"));
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Picture"));
+                this.PropertyChanged(this, new PropertyChangedEventArgs("NextPicture"));
+            }
+            
+            var storyboard = Resources["LeftAnimate"] as Storyboard;
+            if (storyboard != null)
+            {
+                storyboard.Begin();
+            }
+           
+            this.current.Navigate("http://www.cnn.com/video/#/video/bestoftv/2012/09/23/nr-green-day-singer-meltdown-sot.cnn");
+            this.current.Refresh();
+        }
+
+        /// <summary>
+        /// Event implementing INotifyPropertyChanged interface.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Gets a value indicating whether no Kinect is currently connected.
+        /// </summary>
+        public bool IsDisconnected
+        {
+            get
+            {
+                return this.isDisconnectedField;
+            }
+
+            private set
+            {
+                if (this.isDisconnectedField != value)
+                {
+                    this.isDisconnectedField = value;
+
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("IsDisconnected"));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets any message associated with a failure to connect.
+        /// </summary>
+        public string DisconnectedReason
+        {
+            get
+            {
+                return this.disconnectedReasonField;
+            }
+
+            private set
+            {
+                if (this.disconnectedReasonField != value)
+                {
+                    this.disconnectedReasonField = value;
+
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("DisconnectedReason"));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the index number of the image to be shown.
+        /// </summary>
+        public int Index
+        {
+            get
+            {
+                return this.indexField;
+            }
+
+            set
+            {
+                if (this.indexField != value)
+                {
+                    this.indexField = value;
+
+                    // Notify world of change to Index and Picture.
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("Index"));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create a wired-up recognizer for running the slideshow.
+        /// </summary>
+        /// <returns>The wired-up recognizer.</returns>
+        private Recognizer CreateRecognizer()
+        {
+            // Instantiate a recognizer.
+            var recognizer = new Recognizer();
+
+            // Wire-up swipe right to manually advance picture.
+            recognizer.SwipeRightDetected += (s, e) =>
+            {
+                if (e.Skeleton.TrackingId == nearestId)
+                {
+                    Index++;
+
+                    // Setup corresponding picture if pictures are available.
+                    /***************   NEED TO CHANGE PICTURE HERE  &*********/
+
+                    // Notify world of change to Index and Picture.
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("PreviousPicture"));
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("Picture"));
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("NextPicture"));
+                    }
+
+                    var storyboard = Resources["LeftAnimate"] as Storyboard;
+                    if (storyboard != null)
+                    {
+                        storyboard.Begin();
+                    }
+
+                }
+            };
+
+            // Wire-up swipe left to manually reverse picture.
+            recognizer.SwipeLeftDetected += (s, e) =>
+            {
+                if (e.Skeleton.TrackingId == nearestId)
+                {
+                    Index--;
+
+                    // Setup corresponding picture if pictures are available.
+                    /***************   NEED TO CHANGE PICTURE HERE  &*********/
+                    //this.next.Source = new Uri("http://www.cnn.com/video/#/video/bestoftv/2012/09/23/nr-green-day-singer-meltdown-sot.cnn");
+                    //this.previous.Source = new Uri("http://www.cnn.com/video/#/video/bestoftv/2012/09/23/nr-green-day-singer-meltdown-sot.cnn");
+                    //this.current.Source = new Uri("http://www.cnn.com/video/#/video/us/2012/09/21/pkg-hi-tsunami-floating-dock.kgmb-khnl");
+                    this.next.Navigate("http://www.cnn.com/video/#/video/bestoftv/2012/09/23/nr-green-day-singer-meltdown-sot.cnn");
+                    this.previous.Navigate("http://www.cnn.com/video/#/video/bestoftv/2012/09/23/nr-green-day-singer-meltdown-sot.cnn");
+                    this.current.Navigate("http://www.cnn.com/video/#/video/us/2012/09/21/pkg-hi-tsunami-floating-dock.kgmb-khnl");
+
+                    // Notify world of change to Index and Picture.
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("PreviousPicture"));
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("Picture"));
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("NextPicture"));
+                    }
+
+                    var storyboard = Resources["RightAnimate"] as Storyboard;
+                    if (storyboard != null)
+                    {
+                        storyboard.Begin();
+                    }
+
+                }
+            };
+
+            return recognizer;
+        }
+
+        /// <summary>
+        /// Handle insertion of Kinect sensor.
+        /// </summary>
+        private void InitializeNui()
+        {
+            this.UninitializeNui();
+
+            var index = 0;
+            while (this.nui == null && index < KinectSensor.KinectSensors.Count)
+            {
+                try
+                {
+                    this.nui = KinectSensor.KinectSensors[index];
+
+                    this.nui.Start();
+
+                    this.IsDisconnected = false;
+                    this.DisconnectedReason = null;
+                }
+                catch (IOException ex)
+                {
+                    this.nui = null;
+
+                    this.DisconnectedReason = ex.Message;
+                }
+                catch (InvalidOperationException ex)
+                {
+                    this.nui = null;
+
+                    this.DisconnectedReason = ex.Message;
+                }
+
+                index++;
+            }
+
+            if (this.nui != null)
+            {
+                this.nui.SkeletonStream.Enable();
+
+                this.nui.SkeletonFrameReady += this.OnSkeletonFrameReady;
+            }
+        }
+
+        /// <summary>
+        /// Handle removal of Kinect sensor.
+        /// </summary>
+        private void UninitializeNui()
+        {
+            if (this.nui != null)
+            {
+                this.nui.SkeletonFrameReady -= this.OnSkeletonFrameReady;
+
+                this.nui.Stop();
+
+                this.nui = null;
+            }
+
+            this.IsDisconnected = true;
+            this.DisconnectedReason = null;
+        }
+
+        /// <summary>
+        /// Window loaded actions to initialize Kinect handling.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // Start the Kinect system, this will cause StatusChanged events to be queued.
+            this.InitializeNui();
+
+            //thisURL="http://www.cnn.com/video/#/video/us/2012/09/21/pkg-hi-tsunami-floating-dock.kgmb-khnl";
+
+            // Handle StatusChange events to pick the first sensor that connects.
+            KinectSensor.KinectSensors.StatusChanged += (s, ee) =>
+            {
+                switch (ee.Status)
+                {
+                    case KinectStatus.Connected:
+                        if (nui == null)
+                        {
+                            Debug.WriteLine("New Kinect connected");
+
+                            InitializeNui();
+                        }
+                        else
+                        {
+                            Debug.WriteLine("Existing Kinect signalled connection");
+                        }
+
+                        break;
+                    default:
+                        if (ee.Sensor == nui)
+                        {
+                            Debug.WriteLine("Existing Kinect disconnected");
+
+                            UninitializeNui();
+                        }
+                        else
+                        {
+                            Debug.WriteLine("Other Kinect event occurred");
+                        }
+
+                        break;
+                }
+            };
+        }
+
+        /// <summary>
+        /// Handler for skeleton ready handler.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
+        private void OnSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            // Get the frame.
+            using (var frame = e.OpenSkeletonFrame())
+            {
+                // Ensure we have a frame.
+                if (frame != null)
+                {
+                    // Resize the skeletons array if a new size (normally only on first call).
+                    if (this.skeletons.Length != frame.SkeletonArrayLength)
+                    {
+                        this.skeletons = new Skeleton[frame.SkeletonArrayLength];
+                    }
+
+                    // Get the skeletons.
+                    frame.CopySkeletonDataTo(this.skeletons);
+
+                    // Assume no nearest skeleton and that the nearest skeleton is a long way away.
+                    var newNearestId = -1;
+                    var nearestDistance2 = double.MaxValue;
+
+                    // Look through the skeletons.
+                    foreach (var skeleton in this.skeletons)
+                    {
+                        // Only consider tracked skeletons.
+                        if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            // Find the distance squared.
+                            var distance2 = (skeleton.Position.X * skeleton.Position.X) +
+                                (skeleton.Position.Y * skeleton.Position.Y) +
+                                (skeleton.Position.Z * skeleton.Position.Z);
+
+                            // Is the new distance squared closer than the nearest so far?
+                            if (distance2 < nearestDistance2)
+                            {
+                                // Use the new values.
+                                newNearestId = skeleton.TrackingId;
+                                nearestDistance2 = distance2;
+                            }
+                        }
+                    }
+
+                    if (this.nearestId != newNearestId)
+                    {
+                        this.nearestId = newNearestId;
+                    }
+
+                    // Pass skeletons to recognizer.
+                    this.activeRecognizer.Recognize(sender, frame, this.skeletons);
+                }
+            }
+        }
+
+    }
+}
